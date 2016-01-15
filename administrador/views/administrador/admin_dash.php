fe6f851ab2d57848342add96378166ae3ddc8b34
yjqz86 <yeisonquintero@gmail.com>
2016-01-15 16:14:20 -0430

    Nueva vista para administrador

---
+++ administrador/views/administrador/admin_dash.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <div class=""><a href="<?php echo base_url();?>usuario/usuarios">Usuarios</a></div>
+  <div class=""><a href="<?php echo base_url();?>administrador/administradores">Contenido</a></div>
+  <div class=""><a href="<?php echo base_url();?>profesor/profesores">Cursos y/o Talleres</a></div>
+  <div class=""><a href="<?php echo base_url();?>alumno/alumnos">Calendario</a</div>
+  <div class=""><a href="<?php echo base_url();?>institucion/instituciones">Tienda</a></div>
+  <div class=""><a href="<?php echo base_url();?>asignatura/asignaturas">Testimonios</a></div>
+</div>
