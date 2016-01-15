6b7a533484915c85a4de17e02999543b351ddcc0
yjqz86 <yeisonquintero@gmail.com>
2016-01-15 16:17:26 -0430

    Constitucion de corelib, config_var para establacimiento de conexiones con bases de datos y variables globales. Nuevo archivo htaccess y archivo de index el cual invoca al login

---
+++ administrador/config_var.php
@@ -0,0 +1,11 @@
+<?php
+
+    $approot = $_SERVER['DOCUMENT_ROOT']."/adiweb/";
+    $varbase ="http://".$_SERVER['HTTP_HOST']."/adiweb/";
+    
+    $hostname = "apitecdev.com"; 
+    $username = "livehost_adiweb";
+    $password = "adi0102web";
+    $dbname = "livehost_adiweb";
+    
+?>
--- administrador/views/login.php
+++ administrador/views/login.php
@@ -1,4 +1,4 @@
-
+<?php $baseurl="http://www.arturoarellano.com/" ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -33,19 +33,15 @@
   <body>
 
     <div class="container">
-
-      <form class="form-signin" action="<?php echo $baseurl;?>administrador/login/dologin" method="POST">
-        <h2 class="form-signin-heading">Inicia Session</h2>
+      <form class="form-signin" action="<?php echo $baseurl?>administrador/control" method="POST">
+        <h2 class="form-signin-heading">Inicia Sesion</h2>
         <label for="inputEmail" class="sr-only">Usuario</label>
-        <input name="username" type="text" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
+        <input name="username" type="text" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus><br>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required><br>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar</button>
       </form>
-
     </div> <!-- /container -->
-
-
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
