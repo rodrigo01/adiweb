68f351573ed39e668dd67b28bf67c435c2fa41d5
Rodrigo Ornelas <rodrigo01@gmail.com>
2016-01-15 16:37:48 -0800

    cambios nuevos

---
+++ corelib.php
@@ -0,0 +1,20 @@
+<?php
+
+require("config_var.php");
+
+$dbhandle = mysql_connect($hostname, $username, $password) 
+  or die("Unable to connect to MySQL");
+
+$selected = mysql_select_db($dbname,$dbhandle) 
+  or die("Could not select examples");
+
+//Invocacion de modelos por medio de clases
+//include_once("model/main.php");
+
+// Carga de variables de conexion global
+$appControl = 'control';
+$appViews = 'views';
+$appModel = 'model';
+$baseurl = 'http://www.arturoarellano.com/';
+
+?>
