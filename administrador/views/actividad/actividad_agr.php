8fc0f6c6aa0c8e8805ea69b473636d2e15f996aa
yjqz86 <yeisonquintero@gmail.com>
2016-01-21 15:17:23 -0430

    Vista de agr_actividad

--- administrador/views/actividad/actividad_agr.php
+++ administrador/views/actividad/actividad_agr.php
@@ -1,5 +1,5 @@
 	<h3>Agregar Actividad</h3>
-	<form action="<?php echo $baseurl;?>administrador/actividad/addactividad" method="POST" name="form" onSubmit="return validar_clave()">
+	<form action="<?php echo $baseurl;?>administrador/actividad/addactividad" method="POST" enctype="multipart/form-data">
 		<div class="form-group"><div class="row">
 			<div class="col-md-2">Titulo de la Actividad</div>		
 			<div class="col-md-4"> <input type="text" name="nombreActividad" class="form-control" required/></div>
@@ -14,7 +14,7 @@
 		</div>
 		</div><div class="form-group"><div class="row">
 			<div class="col-md-2">Subir Imagen</div>
-			<div class="col-md-4"> <input name="imgActividad" class="form-control" type="file" required/></div>
+			<div class="col-md-4">  <input type="file" name="fileToUpload" id="fileToUpload" required></div>
 		</div>
 		</div><div class="form-group"><div class="row">
 			<div class="col-md-2">Tipo de Actividad</div>
@@ -26,8 +26,30 @@
                             </select>
                         </div>
 		</div>
+		</div><div class="form-group"><div class="row">
+			<div class="col-md-2">Fecha de Inicio de Actividad</div>
+			<div class="col-md-4"> <input name="fechaInicio" class="form-control" type="date" required/></div>
+		</div>
+		</div><div class="form-group"><div class="row">
+			<div class="col-md-2">Fecha de Fin de Actividad</div>
+			<div class="col-md-4"> <input name="fechaFin" class="form-control" type="date" required/></div>
+		</div>
+		</div><div class="form-group"><div class="row">
+			<div class="col-md-2">Fecha de Actividad En Letras (Ejemplo: Desde el 21 al 23 de Enero)</div>
+			<div class="col-md-4"> <input type="text" name="fechaActividad" class="form-control" id="correoUsuario" required/></div>
+		</div>
+		</div><div class="form-group"><div class="row">
+			<div class="col-md-2">Tipo de Actividad</div>
+			<div class="col-md-4"> 
+                            <select name="estadoActividad" class="form-control" id="" required>
+                                <option value="">Seleccionar..</option>
+                                <option value="1">Activa</option>
+                                <option value="0">Inactiva</option>
+                            </select>
+                        </div>
+		</div>
 		</div>
 		<div>
-			<input type="submit" class="btn btn-success" value="Agregar" class="btn btn-succcess" name="agr_actividad"/>
+			<input type="submit" class="btn btn-success" value="Agregar" class="btn btn-succcess" name="submit"/>
 		</div>
 	</form>
