d22bd4aa51e8b76df41663b59ce98f8f2ba50711
yjqz86 <yeisonquintero@gmail.com>
2016-01-21 15:16:49 -0430

    Se añadio la funcion addactividades

--- administrador/models/actividades.php
+++ administrador/models/actividades.php
@@ -1,6 +1,13 @@
 <?php
 
 class Actividades{
+    
+ function AddActividad($nombreActividad,$costoActividad,$contenidoActividad,$imgActividad,$tipoActividad,$fechaActividad,$fechaInicio,$fechaFin,$estadoActividad,$nombreActividad){
+     $Query = "INSERT INTO actividades (nombre_actividad, costo_actividad, contenido_actividad, img_actividad, tipo_actividad, fecha_actividad, fecha_inicio_actividad, fecha_fin_actividad, estado_actividad, seccion_actividad) VALUES ('".$nombreActividad."', '".$costoActividad."', '".$contenidoActividad."', '".$imgActividad."', '".$tipoActividad."', '".$fechaActividad."', '".$fechaInicio."', '".$fechaFin."', '".$estadoActividad."', '".$nombreActividad."')";
+     //die($Query);                
+     $res = mysql_query($Query);       
+     return $res;
+ }
 
  function getActividadXId($id_actividad){
                 $Query="SELECT * FROM actividades WHERE id_actividad= '".$id_actividad."'";
