a4a045cfc50fc189711e8abbd6eac7d863ab5c5a
yjqz86 <yeisonquintero@gmail.com>
2016-01-28 10:21:39 -0430

    Ultimos ajustes

--- administrador/views/producto/producto_agr.php
+++ administrador/views/producto/producto_agr.php
@@ -1,4 +1,11 @@
-	<h3>Agregar Producto</h3>
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script> 
+<script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
+<script>
+    webshims.setOptions('waitReady', false);
+    webshims.setOptions('forms-ext', {types: 'date'});
+    webshims.polyfill('forms forms-ext');    
+</script>
+<h3>Agregar Producto</h3>
 	<form action="<?php echo $baseurl;?>administrador/producto/addproducto" method="POST" enctype="multipart/form-data">
 		<div class="form-group"><div class="row">
 			<div class="col-md-2">Titulo del Producto</div>		
